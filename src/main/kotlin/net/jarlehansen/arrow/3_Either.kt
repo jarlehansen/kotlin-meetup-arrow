821ba68dfbd76c2a884460188c506d1991063292
jarlehansen <jarle@jarlehansen.net>
2020-10-19 18:48:41 +0200

    updated

--- src/main/kotlin/net/jarlehansen/arrow/3_Either.kt
+++ src/main/kotlin/net/jarlehansen/arrow/3_Either.kt
@@ -2,9 +2,13 @@ package net.jarlehansen.arrow
 
 import arrow.core.Either
 
-private suspend fun String.toIntEither(): Either<Throwable, Int> = Either.catch { this.toInt() }
+private suspend fun String.toIntEither(): Either<Throwable, Int> =
+    Either.catch { this.toInt() }
 
 suspend fun main() {
     // fold - Applies ifLeft if this is a Left or ifRight if this is a Right
-    "123".toIntEither().fold({"$it is not a number"}, {"Number is $it"}).also { println(it) }
+
+    "123".toIntEither()
+        .fold({"$it is not a number"}, {"Number is $it"})
+        .also { println(it) }
 }
